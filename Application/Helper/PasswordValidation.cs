902dd11f1f94ab86a71cb395f237eb264d066cb7
github-Krunal <githubkrunal@gmail.com>
2021-03-05 01:53:02 +0530

    3 consective value validation

---
+++ Application/Helper/PasswordValidation.cs
@@ -0,0 +1,21 @@
+﻿
+using Application.Models;
+
+namespace Application.Helper
+{
+    public class PasswordValidation
+    {
+        public bool validation(Registration registration)
+        {
+            for (int i = 0; (i+2) < registration.Username.Length; i++)
+            {
+                if (registration.Password.Contains(registration.Username.Substring(i, 3)))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
